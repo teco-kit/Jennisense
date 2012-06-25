c0e36276953073be910472b4aebf7ec1d169f3ef
Philipp Scholl <pscholl@bawue.de>
2012-06-25 16:17:21 +0200

    add button sensor example
     shows how to listen and react on pushbutton events

---
+++ applications/button-sensor-sample/button-sensor-sample.c
@@ -0,0 +1,33 @@
+#include "contiki.h"
+#include "dev/button-sensor.h"
+
+/*---------------------------------------------------------------------------*/
+PROCESS(button_serial, "button_serial");
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(button_serial, ev, data)
+{
+  static struct sensors_sensor *btn;
+  static struct etimer et;
+
+  PROCESS_BEGIN();
+  uart0_set_br(1000000);
+  PROCESS_PAUSE();
+
+  btn = sensors_find(BUTTON_SENSOR);
+
+  if (btn!=NULL)
+    SENSORS_ACTIVATE(*btn);
+
+  etimer_set(&et, CLOCK_SECOND/2);
+
+  while(1) {
+    PROCESS_YIELD_UNTIL(ev==sensors_event);
+    printf("btn2: %d\n", btn->value(BUTTON_0));
+    etimer_reset(&et);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+AUTOSTART_PROCESSES(&button_serial);
+/*---------------------------------------------------------------------------*/
