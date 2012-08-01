cfc499753ce16471e475b5d4df902fc3bc8200d3
Philipp Scholl <pscholl@bawue.de>
2012-08-01 10:51:54 +0200

    fix hello world example
    Thanks Ant Skelton

--- applications/hello-world-leds/hello-world-leds.c
+++ applications/hello-world-leds/hello-world-leds.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * Copyright (c) 2012, TU Darmstadt.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,21 +27,11 @@
  * SUCH DAMAGE.
  *
  * This file is part of the Contiki operating system.
- *
- * $Id: hello-world.c,v 1.1 2006/10/02 21:46:46 adamdunkels Exp $
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
  */
 
 #include "contiki.h"
 #include "dev/leds.h"
 
-#include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
@@ -51,20 +41,21 @@ AUTOSTART_PROCESSES(&hello_world_process);
 
 PROCESS_THREAD(hello_world_process, ev, data)
 {
-  int i=0;
   static struct etimer period_timer;
 
   PROCESS_BEGIN();
   PROCESS_PAUSE();
-  
+
+  leds_on(LEDS_ALL);
   etimer_set(&period_timer, INTERVAL);
 
   while(1){
-	PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_TIMER);
-	etimer_set(&period_timer, INTERVAL);
-	if(i^=0)leds_arch_set(0); else leds_arch_set(LEDS_ALL);
+    PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_TIMER);
+
+    leds_toggle(LEDS_ALL);
+    etimer_restart(&period_timer);
   }
-  
+
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
