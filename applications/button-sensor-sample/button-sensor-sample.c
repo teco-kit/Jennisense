393cda81971b6f7a972a99fa359743ee13ae02da
Markus Scholz <mr_inbox@onlinehome.de>
2013-02-07 18:26:46 +0100

    * clarified button example to show change of button state

--- applications/button-sensor-sample/button-sensor-sample.c
+++ applications/button-sensor-sample/button-sensor-sample.c
@@ -10,7 +10,7 @@ PROCESS_THREAD(button_serial, ev, data)
   static struct etimer et;
 
   PROCESS_BEGIN();
-  uart0_set_br(1000000);
+  
   PROCESS_PAUSE();
 
   btn = sensors_find(BUTTON_SENSOR);
@@ -19,11 +19,16 @@ PROCESS_THREAD(button_serial, ev, data)
     SENSORS_ACTIVATE(*btn);
 
   etimer_set(&et, CLOCK_SECOND/2);
+  static int pressed = 0;
 
   while(1) {
     PROCESS_YIELD_UNTIL(ev==sensors_event && data==btn);
-    printf("btn2: %d\n", btn->value(BUTTON_0));
-    etimer_reset(&et);
+    
+    if(btn->value(BUTTON_0)!=pressed){
+        printf("btn0 pressed/released \n");
+        pressed=btn->value(BUTTON_0);
+    } 
+     etimer_reset(&et);
   }
 
   PROCESS_END();
